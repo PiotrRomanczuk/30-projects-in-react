7056070ac662038fc09327e5e1629c00397b6c64
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-02-12 00:55:35 +0100

    Adding bootstrap for buttons

--- src/projects/02-randomizeColors/RandomizeColors.jsx
+++ src/projects/02-randomizeColors/RandomizeColors.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Title from '../components/Title';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function RandomizeColors() {
 	return (
-		<div className='container'>
+		<div className='container gap-4'>
 			<Title text={'Randomize Colors'} classes={'mb-4'} />
-			<button>Click me!</button>
+			<Button variant='primary mr-3'>Primary</Button>
+			<Button className='primary mr-5 '>Primary</Button>
+			<Button variant='primary'>Primary</Button>
+			<Button variant='primary'>Primary</Button>
+			<Button variant='primary'>Primary</Button>
 		</div>
 	);
 }
