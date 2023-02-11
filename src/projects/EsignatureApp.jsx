1e8f28695f0ff74ba567c5b22fafb674bfc46a0d
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-02-11 21:09:14 +0100

    Added inputs in footer + inputStyles

--- src/projects/EsignatureApp.jsx
+++ src/projects/EsignatureApp.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import Title from './components/Title';
 
 export default function EsignatureApp() {
+	const inputStyle = {
+		border: 'none',
+		borderBottom: '2px dotted',
+		outline: 'none',
+		padding: '.035rem 0',
+	};
+
 	return (
 		<div className='container text-center'>
 			<Title classes={'title'} text={'Piotr'} />
-			<Title classes={'subtitle'} text={'Date'} />
+			<Title classes={'subtitle mb-4'} text={'Date'} />
 			<p>
 				Lorem Ipsum is simply dummy text of the printing and typesetting
 				industry. Lorem Ipsum has been the industry's standard dummy text ever
@@ -17,6 +24,18 @@ export default function EsignatureApp() {
 				with desktop publishing software like Aldus PageMaker including versions
 				of Lorem Ipsum.
 			</p>
+
+			<footer
+				className='d-flex'
+				style={{
+					justifyContent: 'space-around',
+					position: 'relative',
+					top: '40vh',
+				}}
+			>
+				<input type='date' value={''} style={inputStyle}></input>
+				<input type='text' value={''} style={inputStyle}></input>
+			</footer>
 		</div>
 	);
 }
