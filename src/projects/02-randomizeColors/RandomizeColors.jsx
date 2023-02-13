c1b4482aaaea77732977c2d4619308b40f608c03
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-02-13 17:17:33 +0100

    Adding addEventListener to the buttons

--- src/projects/02-randomizeColors/RandomizeColors.jsx
+++ src/projects/02-randomizeColors/RandomizeColors.jsx
@@ -1,17 +1,58 @@
 import React from 'react';
 import Title from '../components/Title';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function RandomizeColors() {
+	function handleClick(e) {
+		// console.log('click ');
+		console.log(e.target.id);
+	}
+
 	return (
 		<div className='container gap-4'>
 			<Title text={'Randomize Colors'} classes={'mb-4'} />
-			<Button variant='primary mr-3'>Primary</Button>
-			<Button className='primary mr-5 '>Primary</Button>
-			<Button variant='primary'>Primary</Button>
-			<Button variant='primary'>Primary</Button>
-			<Button variant='primary'>Primary</Button>
+
+			<button
+				type='button'
+				className='btn btn-primary'
+				id='1'
+				onClick={handleClick}
+			>
+				Click me!
+			</button>
+			<button
+				type='button'
+				className='btn btn-secondary'
+				id='2'
+				onClick={handleClick}
+			>
+				Click me!
+			</button>
+			<button
+				type='button'
+				className='btn btn-success'
+				id='3'
+				onClick={handleClick}
+			>
+				Click me!
+			</button>
+			<button
+				type='button'
+				className='btn btn-danger'
+				id='4'
+				onClick={handleClick}
+			>
+				Click me!
+			</button>
+			<button
+				type='button'
+				className='btn btn-warning'
+				id='5'
+				onClick={handleClick}
+			>
+				Click me!
+			</button>
 		</div>
 	);
 }
