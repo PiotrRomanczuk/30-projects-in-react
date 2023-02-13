9e77c7c66d97c772662d6657c5bffec1b111f12e
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-02-13 20:46:18 +0100

    Setting up a card container in css

--- src/projects/03-likeMyPhoto/likePhotoApp.jsx
+++ src/projects/03-likeMyPhoto/likePhotoApp.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import Title from '../components/Title';
-// import Button from 'react-bootstrap/Button';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import { IconName } from 'react-icons/fa';
 
 export default function LikePhotoApp() {
 	return (
 		<div className='container'>
 			<Title text={'Like Photo App'} classes={'mb-3'} />
 			<Title text={'likes'} classes={'subtitle'} />
+
+			<div
+				className='card card-dark m-auto'
+				style={{ width: 300, cursor: 'pointer' }}
+			>
+				<div className='card-header fs-xl'>
+					<small>DogyDog</small>
+				</div>
+				<img src='' alt='' style={{ height: 'fit-content' }}></img>
+				<div
+					className='card-footer fs-xl d-flex'
+					style={{
+						justifyContent: 'space-between',
+					}}
+				></div>
+			</div>
 		</div>
 	);
 }
