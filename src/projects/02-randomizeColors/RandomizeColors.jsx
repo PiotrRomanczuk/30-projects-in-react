5b545ce354077ff33fd910ffdd90e7be57420d66
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-02-13 17:36:57 +0100

    Finishing project nb.2

--- src/projects/02-randomizeColors/RandomizeColors.jsx
+++ src/projects/02-randomizeColors/RandomizeColors.jsx
@@ -4,53 +4,46 @@ import Title from '../components/Title';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function RandomizeColors() {
+	function getRandomColor() {
+		let letters = '0123456789ABCDEF';
+		let color = '#';
+
+		for (let i = 0; i < 6; i++) {
+			color += letters[Math.floor(Math.random() * 16)];
+		}
+
+		return color;
+	}
+
 	function handleClick(e) {
 		// console.log('click ');
 		console.log(e.target.id);
+		console.log(getRandomColor());
+
+		let body = document.querySelector('body');
+
+		console.log(body.style.backgroundColor);
+
+		body.style.backgroundColor = getRandomColor;
 	}
 
 	return (
-		<div className='container gap-4'>
+		<div className='container gap-4' onClick={handleClick}>
 			<Title text={'Randomize Colors'} classes={'mb-4'} />
 
-			<button
-				type='button'
-				className='btn btn-primary'
-				id='1'
-				onClick={handleClick}
-			>
+			<button type='button' className='btn btn-primary' id='1'>
 				Click me!
 			</button>
-			<button
-				type='button'
-				className='btn btn-secondary'
-				id='2'
-				onClick={handleClick}
-			>
+			<button type='button' className='btn btn-secondary' id='2'>
 				Click me!
 			</button>
-			<button
-				type='button'
-				className='btn btn-success'
-				id='3'
-				onClick={handleClick}
-			>
+			<button type='button' className='btn btn-success' id='3'>
 				Click me!
 			</button>
-			<button
-				type='button'
-				className='btn btn-danger'
-				id='4'
-				onClick={handleClick}
-			>
+			<button type='button' className='btn btn-danger' id='4'>
 				Click me!
 			</button>
-			<button
-				type='button'
-				className='btn btn-warning'
-				id='5'
-				onClick={handleClick}
-			>
+			<button type='button' className='btn btn-warning' id='5'>
 				Click me!
 			</button>
 		</div>
