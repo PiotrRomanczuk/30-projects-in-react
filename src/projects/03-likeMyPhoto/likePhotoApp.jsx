86cb4a9a82147b3c7ced7c67deb9a0dabb9ddae3
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-02-13 17:45:10 +0100

    Creating new project

---
+++ src/projects/03-likeMyPhoto/likePhotoApp.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Title from '../components/Title';
+// import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function likePhotoApp() {
+	return <div>likePhotoApp</div>;
+}
